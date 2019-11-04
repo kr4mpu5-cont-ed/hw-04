f3dccfbdc15561b1dbe530cfbc60bfadd0dd359a
kr4mpu5 <derkrampus@icloud.com>
2019-11-04 00:27:49 -0600

    meets all functional reqs, plus a bonus req, changes visual style

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,8 @@
-var mainEl = document.getElementById("main");
-var timeEl = document.getElementById("time");
+//todo: refactor global variables to local as appropriate
+var timeLeft = document.getElementById("timeLeft");
 var startButton = document.getElementById("start");
+var navBarDiv = document.getElementById("navBar");
+var viewHighscoresButton = document.getElementById("viewHighscores");
 var startDiv = document.getElementById("startDiv");
 var questionDiv = document.getElementById("questionDiv");
 var resultDiv = document.getElementById("resultDiv");
@@ -12,35 +14,46 @@ var questionDivChoice0 = document.getElementById("questionDivChoice0");
 var questionDivChoice1 = document.getElementById("questionDivChoice1");
 var questionDivChoice2 = document.getElementById("questionDivChoice2");
 var questionDivChoice3 = document.getElementById("questionDivChoice3");
-var secondsLeft = 75;
+var submitScoreButton = document.getElementById("submitScore");
+var startOverButton = document.getElementById("startOver");
+var clearHighscoresButton = document.getElementById("clearHighscores");
+const totalNumberQuestions = questions.length - 1; //0-based array
+var secondsLeft = questions.length * 15; //15 seconds/question
 var currentQuestion = 0;
 var timerInterval;
 var timerFlashResult;
 var secondsLeftFlashResult = 2;
-const totalNumberQuestions = questions.length - 1; //0-based array
+var scores = [];
+
+init();
+
+function init() {
+    //set initial display state of various divs
+    navBarDiv.setAttribute("style", "visibility: visible;");
+    startDiv.setAttribute("style", "display");
+    questionDiv.setAttribute("style", "display: none;");
+    resultDiv.setAttribute("style", "display: none;");
+    gameOverDiv.setAttribute("style", "display: none;");
+    highscoresDiv.setAttribute("style", "display: none;");
+
+    //populate scores[] fromlocalstorage
+    getScoresLS = localStorage.getItem("scoresLS");
+    scores = getScoresLS ? JSON.parse(getScoresLS): [];
+}
 
 function startQuiz() {
     startTimer();
     displayNextQuestion(currentQuestion);
 }
 
-function endQuiz() {
-    questionDiv.setAttribute("style", "display:none");
-    resultDiv.setAttribute("style", "display:none");
-    score.innerHTML = secondsLeft;
-    timeEl.textContent = "Time: " + secondsLeft;
-    gameOverDiv.setAttribute("style", "display:");
-    clearInterval(timerInterval);
-}
-
 function startTimer() {
     timerInterval = setInterval(function() {
     secondsLeft--;
 
-    timeEl.textContent = "Time: " + secondsLeft;
+    timeLeft.textContent = "Time: " + secondsLeft;
 
     if(secondsLeft === 0) {
-        console.log("Time has run out.")
+        // console.log("Time has run out.")
         clearInterval(timerInterval);
         endQuiz();
     }
@@ -48,8 +61,18 @@ function startTimer() {
   }, 1000);
 }
 
-function displayNextQuestion(int) {
-    startDiv.setAttribute("style", "display: none");
+function endQuiz() {
+    //hide question/result divs, show game over div
+    questionDiv.setAttribute("style", "display: none;");
+    resultDiv.setAttribute("style", "display: none;");
+    score.innerHTML = secondsLeft;
+    timeLeft.textContent = "Time: " + secondsLeft;
+    gameOverDiv.setAttribute("style", "display");
+    clearInterval(timerInterval);
+}
+
+function displayNextQuestion() {
+    startDiv.setAttribute("style", "display: none;");
     questionDiv.setAttribute("style", "display");
 
     questionDivTitle.innerHTML = questions[currentQuestion].title;
@@ -70,6 +93,7 @@ function checkResponse(str) {
         secondsLeft = secondsLeft - 10;
         if (secondsLeft < 0) {
             secondsLeft = 0;
+            endQuiz();
         }
         resultDiv.innerHTML = "Incorrect!";
     }
@@ -78,7 +102,7 @@ function checkResponse(str) {
     timerFlashResult = setInterval(function() {
         secondsLeftFlashResult--;
         if (secondsLeftFlashResult === 0) {
-            resultDiv.setAttribute("style", "display: none");
+            resultDiv.setAttribute("style", "display: none;");
             clearInterval(timerFlashResult);
         }    
     }, 1000);
@@ -91,6 +115,72 @@ function checkResponse(str) {
         endQuiz();
     }    
 }
+
+function storeScores() {
+    //hide divs not necessary for highscore screen
+    navBarDiv.setAttribute("style", "visibility: hidden;");
+    gameOverDiv.setAttribute("style", "display: none;");
+    highscoresDiv.setAttribute("style", "display");
+
+    //get values from player-submitted form
+    strPlayerInitials = document.getElementById("playerInitials").value.trim();
+    strScore = secondsLeft;
+    
+    //add to scores[]
+    scores.push([strPlayerInitials, strScore]);
+
+    //sort scores, high to low
+    scores.sort(function(a,b) {
+        return b[1] - a[1];
+    });
+
+    //write to localstorage
+    localStorage.setItem('scoresLS', JSON.stringify(scores));
+
+    //display high scores
+    renderHighscores();
+
+}
+
+function getHighscores() {
+    var storedScores = JSON.parse(localStorage.getItem("todos"));
+
+    // If todos were retrieved from localStorage, update the todos array to it
+    if (storedTodos !== null) {
+      todos = storedTodos;
+    }
+  
+    // Render todos to the DOM
+    renderTodos();
+}
+
+function renderHighscores() {
+    // Clear highscoreList element
+    var highscoreList = document.getElementById("highScoreList");
+    highscoreList.innerHTML = "";
+  
+    // Render a new li for each score
+    for (var i = 0; i < scores.length; i++) {
+        var score = scores[i];
+        console.log(scores[i] + " " + score);
+        var li = document.createElement("li");
+        li.textContent = [i+1] + ". " + scores[i][0].toString().toUpperCase() + " - " + scores[i][1];
+        li.setAttribute("data-index", i);
+        highscoreList.appendChild(li);
+    }
+}
+
+function viewHighscores() {
+    navBarDiv.setAttribute("style", "visibility: hidden;");
+    startDiv.setAttribute("style", "display: none;");
+    highscoresDiv.setAttribute("style", "display");
+    renderHighscores();
+}
+
+
+viewHighscoresButton.addEventListener("click", function() {
+    viewHighscores();
+});
 
 startButton.addEventListener("click", function() {
     startQuiz();
@@ -110,4 +200,21 @@ questionDivChoice2.addEventListener("click", function() {
 
 questionDivChoice3.addEventListener("click", function() {
     checkResponse(questionDivChoice3.getAttribute("data-choice"));
+});
+
+submitScoreButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    storeScores();
+});
+
+startOverButton.addEventListener("click", function() {
+});
+
+clearHighscoresButton.addEventListener("click", function(event) {
+    // localStorage.clear();
+    event.preventDefault();
+    scores = [];
+    localStorage.removeItem('scoresLS');
+
+    renderHighscores();
 });
